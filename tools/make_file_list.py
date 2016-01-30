ae830e1e0d0689a15d58a566ac53d7d5727ce3c3
Josh <josh@skyd.io>
2016-01-30 13:48:37 -0800

    start analysis script

---
+++ tools/make_file_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""Given an access log as created by logfs_fuse, merge all file accesses into a list
+   of files accessed without duplicates, and write that list to a new file."""
+
+import argparse
+
+def main():
+	parser = argpares.ArgumentParser()
+	args = parser.parse_args()
+
+if __name__ == '__main__':
+	main()
